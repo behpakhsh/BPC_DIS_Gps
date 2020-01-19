fa395980ae63b4d16badd814fc6b09825ec19b86
Behpakhsh It Department <behpakhsh.it@gmail.com>
2020-01-19 13:50:15 +0330

    Fix Bug Mock Location

--- gps/src/main/java/bpc/dis/gps/GpsSettingHelper/GpsSettingHelper.java
+++ gps/src/main/java/bpc/dis/gps/GpsSettingHelper/GpsSettingHelper.java
@@ -19,6 +19,9 @@ import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import bpc.dis.gps.GpsTrackerStatus;
+import bpc.dis.gps.Service.GpsTracker;
+
 public class GpsSettingHelper {
 
     private Context context;
@@ -41,46 +44,56 @@ public class GpsSettingHelper {
         builder.setAlwaysShow(true);
     }
 
-    public void turnGPSOn(final onGpsListener onGpsListener) {
+    public void turnGPSOn(GpsSettingHelperListener GpsSettingHelperListener) {
         if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            if (onGpsListener != null) {
-                onGpsListener.gpsStatus(true);
+            if (new GpsTracker(context).getGpsTrackerStatus() == GpsTrackerStatus.IS_FAKE_LOCATION) {
+                openSetting(GpsSettingHelperListener);
+            } else {
+                if (GpsSettingHelperListener != null) {
+                    GpsSettingHelperListener.gpsStatus(true);
+                }
             }
         } else {
-            mSettingsClient
-                    .checkLocationSettings(mLocationSettingsRequest)
-                    .addOnSuccessListener((Activity) context, new OnSuccessListener<LocationSettingsResponse>() {
-                        @Override
-                        public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-                            if (onGpsListener != null) {
-                                onGpsListener.gpsStatus(true);
-                            }
-                        }
-                    })
-                    .addOnFailureListener((Activity) context, new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            int statusCode = ((ApiException) e).getStatusCode();
-                            switch (statusCode) {
-                                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                                    try {
-                                        ResolvableApiException rae = (ResolvableApiException) e;
-                                        rae.startResolutionForResult((Activity) context, reqCode);
-                                    } catch (IntentSender.SendIntentException ee) {
-                                        ee.printStackTrace();
-                                    }
-                                    break;
-                                case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                                    String errorMessage = "Location settings are inadequate, and cannot be " + "fixed here. Fix in Settings.";
-                                    Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    });
+            openSetting(GpsSettingHelperListener);
         }
     }
 
-    public interface onGpsListener {
-        void gpsStatus(boolean isGPSEnable);
+    private void openSetting(final GpsSettingHelperListener GpsSettingHelperListener) {
+        mSettingsClient.checkLocationSettings(mLocationSettingsRequest)
+                .addOnSuccessListener((Activity) context, new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        if (GpsSettingHelperListener != null) {
+                            GpsSettingHelperListener.gpsStatus(true);
+                        }
+                    }
+                })
+                .addOnFailureListener((Activity) context, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                try {
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult((Activity) context, reqCode);
+                                } catch (IntentSender.SendIntentException ee) {
+                                    ee.printStackTrace();
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                String errorMessage = "Location settings are inadequate, and cannot be " + "fixed here. Fix in Settings.";
+                                Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
+
+    public interface GpsSettingHelperListener {
+
+        void gpsStatus(boolean isGPSEnable);
+
+    }
+
 
 }
